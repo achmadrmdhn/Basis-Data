4970d2faf87d032e0f1d474d70340f3fa50c9f18
achmadrmdhn <achmadrifai0511@gmail.com>
2024-04-19 06:47:04 +0700

    updated

--- Tugas-1/dbpegawai.sql
+++ Tugas-1/dbpegawai.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 18 Apr 2024 pada 16.58
+-- Waktu pembuatan: 18 Apr 2024 pada 18.31
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
